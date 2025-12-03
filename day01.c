fcf208720d863ba230588cd085e8f499aea75674
svenhuster <github@lab7b.com>
2025-12-03 14:40:19 +0100

    update formatting

--- day01.c
+++ day01.c
@@ -8,51 +8,47 @@
 
 int main(int argc, char** argv)
 {
-  (void)argc;
-  (void)argv;
+	(void)argc;
+	(void)argv;
 
-  char direction;
-  int steps;
+	char direction;
+	int steps;
 
-  int position = 50;
-  int zero_count = 0;
+	int position = 50;
+	int zero_count = 0;
 
-  while (scanf("%c%d\n", &direction, &steps) == 2) {
-    switch (AOCPART) {
-    case 1: 			// part 1
-      if (direction == 'L') {
-	position -= steps;
-      } else {
-	position += steps;
-      }
+	while (scanf("%c%d\n", &direction, &steps) == 2) {
+		switch (AOCPART) {
+		case 1:		/* part 1 */
+			if (direction == 'L') {
+				position -= steps;
+			} else {
+				position += steps;
+			}
 
-      position = ((position % 100) + 100) % 100;
+			position = ((position % 100) + 100) % 100;
 
-      if (position == 0)
-        ++zero_count;
-      break;
+			if (position == 0)
+				++zero_count;
+			break;
 
-    case 2:			// part 2
-      zero_count += steps / 100;
-      steps %= 100;
+		case 2:		/* part 2 */
+			zero_count += steps / 100;
+			steps %= 100;
 
-      if (direction == 'L') {
-	if (position != 0 && position <= steps)
-	  ++zero_count;
-	position -= steps;
-      } else {
-	if (100 - position <= steps)
-	  ++zero_count;
-	position += steps;
-      }
-
-      position = ((position % 100) + 100) % 100;
-      break;
-
-    }
-
-    DPRINT("%c %d => %d / %d\n", direction, steps, position, zero_count);
-  }
-
-  printf("%d\n", zero_count);
+			if (direction == 'L') {
+				if (position != 0 && position <= steps)
+					++zero_count;
+				position -= steps;
+			} else {
+				if (100 - position <= steps)
+					++zero_count;
+				position += steps;
+			}
+			position = ((position % 100) + 100) % 100;
+			break;
+		}
+		DPRINT("%c %d => %d / %d\n", direction, steps, position, zero_count);
+	}
+	printf("%d\n", zero_count);
 }
--- day02.c
+++ day02.c
@@ -9,55 +9,54 @@
 
 int main(int argc, char** argv)
 {
-  (void)argc;
-  (void)argv;
+	(void)argc;
+	(void)argv;
 
-  long long start, end;
-  long long total = 0;
+	long long start, end;
+	long long total = 0;
 
-  while (scanf("%lld-%lld,", &start, &end) == 2) {
-    switch (AOCPART) {
-    case 1: 			// part 1
-      for (long long i = start; i <= end; ++i) {
-	int len = (int)log10((double)i) + 1;
-	if (len % 2 == 0) {
-	  long long mask = (long long)pow(10, len/2);
-	  long long first = i / mask;
-	  long long second = i % mask;
-	  if (first == second)
-	    total += (long long)i;
-	  DPRINT("%lld %lld %lld %d\n", i, first, second, first == second);
+	while (scanf("%lld-%lld,", &start, &end) == 2) {
+		switch (AOCPART) {
+		case 1:		/* part 1 */
+			for (long long i = start; i <= end; ++i) {
+				int len = (int)log10((double)i) + 1;
+				if (len % 2 == 0) {
+					long long mask = (long long)pow(10, len/2);
+					long long first = i / mask;
+					long long second = i % mask;
+					if (first == second)
+						total += (long long)i;
+					DPRINT("%lld %lld %lld %d\n", i, first, second, first == second);
+				}
+			}
+			break;
+
+		case 2:		/* part 2 */
+			for (long long i = start; i <= end; ++i) {
+				int len = (int)log10((double)i) + 1;
+				int success = 0;
+				for (int recur = 1; recur <= len/2 && !success; ++recur) {
+					if (len % recur == 0) {
+						long long num = i;
+						long long match = num % (long long)pow(10, recur);
+
+						long long rem;
+						while (num > 0) {
+							rem = num % (long long)pow(10, recur);
+							if (rem != match)
+								goto FAILED;
+							num /= (long long)pow(10, recur);
+						}
+						total += i;
+						success = 1;
+						DPRINT("i:%lld rem:%lld\n", i, rem);
+FAILED:
+						;
+					}
+				}
+			}
+			break;
+		}
 	}
-      }
-      break;
-
-    case 2:			// part 2
-      for (long long i = start; i <= end; ++i) {
-	int len = (int)log10((double)i) + 1;
-	int success = 0;
-	for (int recur = 1; recur <= len/2 && !success; ++recur) {
-	  if (len % recur == 0) {
-	    long long rem;
-	    long long num = i;
-	    long long match = num % (long long)pow(10, recur);
-	    while (num > 0) {
-	      rem = num % (long long)pow(10, recur);
-	      if (rem != match)
-		goto FAILED;
-	      num /= (long long)pow(10, recur);
-	    }
-	    total += i;
-	    success = 1;
-	    DPRINT("i:%lld rem:%lld\n", i, rem);
-	  FAILED:
-	    ;
-	  }
-	}
-      }
-      break;
-
-    }
-  }
-
-  printf("%lld\n", total);
+	printf("%lld\n", total);
 }
